1d96244834b82d26fc6ba934d82d70dad71ba3bc
Vishnu Nittoor <nitvishn@gmail.com>
2023-02-26 15:47:05 -0500

    runge cutta

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ let solutionPoints = [];
 let solutionCurves = [];
 
 function normalisedDifferenceVector(source, dest){
-    var v = dest.sub(source);
+    var v = dest;
     var length = v.mag();
     v.x = v.x * normalizedLength / length;
     v.y = v.y * normalizedLength / length;
@@ -21,7 +21,7 @@ function p5ToCartesian(p5Vector){
 }
 
 function f(t, X){
-    return createVector(X.y, - X.x);
+    return createVector(- X.y, X.x);
 }
 
 function setup() {
@@ -32,19 +32,30 @@ function setup() {
     zero = createVector(windowWidth/2, windowHeight/2);
 }
 
+function drawArrowAt(x, y){
+    var source = createVector(x, y);
+    var dest = f(0, source);
+    var v = normalisedDifferenceVector(source, dest);
+    point(x, y);
+    line(x, y, x + v.x, y + v.y);
+}
+
 function draw() {
     translate(windowWidth / 2, windowHeight / 2);
     background(0);
     stroke(100);
+    strokeWeight(1);
     for(var x = -xRange; x < xRange; x+=gridSize){
         for(var y = -yRange; y < yRange; y+=gridSize){
-            var source = createVector(x, y);
-            var dest = f(0, source);
-            var v = normalisedDifferenceVector(source, dest);
-            point(x, y);
-            line(x, y, x + v.x, y + v.y);
+            drawArrowAt(x, y);
         }
     }
+
+    // stroke(255);
+    // strokeWeight(4);
+    // drawArrowAt(mouseX - zero.x, mouseY - zero.y);
+    // console.log(f(0, createVector(1, 0)))
+
     for(var i = 0; i < solutionPoints.length; i++){
         solutionPoints[i].update(0.01);
         solutionPoints[i].show();
@@ -54,16 +65,17 @@ function draw() {
         solutionCurves[i].update(0.01);
         solutionCurves[i].show();
     }
+
+    strokeWeight(1);
     // draw the y axis
-    stroke(255)
+    stroke(150)
     line(0, -yRange, 0, yRange);
     // draw the x axis
-    stroke(255);
     line(-xRange, 0, xRange, 0);
     stroke(255);
 }
 
 function mousePressed(){
-    solutionPoints.push(new Solution(mouseX - zero.x, mouseY - zero.y, f, false, true));
-    // solutionCurves.push(new SolutionCurve(mouseX - zero.x, mouseY - zero.y, f));
+    // solutionPoints.push(new Solution(mouseX - zero.x, mouseY - zero.y, f, false, true));
+    solutionCurves.push(new SolutionCurve(mouseX - zero.x, mouseY - zero.y, f));
 }
